dfa1538407da560196484d4401c405259c5cc11e
Eduard.Streltsov <streltsov.eduard@gmail.com>
2019-05-10 19:12:10 +0300

    02.06

--- guest-app/src/main/resources/data.sql
+++ guest-app/src/main/resources/data.sql
@@ -1,2 +1,8 @@
-insert into user (id, user_name, password) values ('1', 'user1', '$2a$11$dp4wMyuqYE3KSwIyQmWZFeUb7jCsHAdk7ZhFc0qGw6i5J124imQBi');
-insert into user (id, user_name, password) values ('2', 'user2', '$2y$11$GllYXZ/zoqnrVaIee5DqsOOAJy32mpmMf46Kne.vN6CV9wkWZ2WSq');
+insert into user (user_name, password) values ('user1', '$2a$11$dp4wMyuqYE3KSwIyQmWZFeUb7jCsHAdk7ZhFc0qGw6i5J124imQBi');
+insert into user (user_name, password) values ('user2', '$2a$11$3NO32OV1TGjap3xMpAEjmuiizitWuaSwUYz42aMtlxRliwJ8zm4Sm');
+-- insert into auth_user_group (id, user_name, auth_group) values ('1', 'user1', 'user');
+-- insert into auth_user_group (id, user_name, auth_group) values ('2', 'user1', 'admin');
+-- insert into auth_user_group (id, user_name, auth_group) values ('3', 'user2', 'user');
+insert into auth_user_group ( user_name, auth_group) values ('user1', 'user');
+insert into auth_user_group ( user_name, auth_group) values ('user1', 'admin');
+insert into auth_user_group ( user_name, auth_group) values ('user2', 'user');
