f43c28b50a212f74f22421644b5355ea71191215
Eduard.Streltsov <streltsov.eduard@gmail.com>
2019-05-10 19:57:00 +0300

    02.09

--- guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -46,15 +47,24 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		//@formatter:off
 		http
 				.csrf().disable()
 				.authorizeRequests()
 				.antMatchers("/", "/index", "/css/*", "/js/*", "/h2-console", "/h2-console/*").permitAll()
 				.anyRequest().authenticated()
 				.and()
-				.httpBasic();
+//				.httpBasic();
+				.formLogin().loginPage("/login").permitAll()
+				.and()
+				.logout()
+				.invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/logout-success").permitAll();
 		
 		http.headers().frameOptions().disable();
+		//@formatter:on
 	}
 	
 	//	@Bean
